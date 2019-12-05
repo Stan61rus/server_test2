0fa17df8d7502285f565b977f25c8f01139e358b
Konstantin Gorb <gorb@ucoz-team.net>
2019-12-05 13:51:10 +0300

    test

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");  
 const app = express();
 
+
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
 app.get('/', function (req, res) {
-    
+  res.render('index.hbs');
   });
 
 app.listen(3000, function () {

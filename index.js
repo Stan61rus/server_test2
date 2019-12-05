1f1fa577355b5b2c9f9a465cd4e810d5a65f776c
Konstantin Gorb <gorb@ucoz-team.net>
2019-12-05 16:16:50 +0300

    Mongo

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const express = require("express");  
 const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
-app.set('views', './views');
-app.set('view engine', 'hbs');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
-  res.render('index.hbs');
-  });
+// Подключаемся к БД
+mongoose.connect('mongodb+srv://stan61rus:753538@cluster0-pmcuq.mongodb.net/test', {useNewUrlParser: true})
+  .then(() => console.log('MongoDB start'))
+  .catch(err => console.log(err));
+
+// Настройки конструктора Handlebars
+app.set('views', './views');
+app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
   res.render('index.hbs');
   });
 
-  app.post('/', function (req, res) {
-    console.log(req.body)
-    res.end()
-    });
+app.post('/', function (req, res) {
+  console.log(req.body)
+  res.end()
+  });
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');

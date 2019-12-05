df2b1469e36e54d5f94460370c66bd41278a4e3d
Konstantin Gorb <gorb@ucoz-team.net>
2019-12-05 13:35:04 +0300

    Tets

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', function (req, res) {
-    const filename = req.query.filename || "file"
-    res.download('file.doc', filename + ".doc")
+    
   });
 
 app.listen(3000, function () {

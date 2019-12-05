f0304d6590b6b0e6c35f3ff4f6be03ecae5788f5
Konstantin Gorb <gorb@ucoz-team.net>
2019-12-05 15:23:29 +0300

    Получаем данные

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require("express");  
 const app = express();
-
+const bodyParser = require('body-parser');
 
 app.set('views', './views');
 app.set('view engine', 'hbs');
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.get('/', function (req, res) {
   res.render('index.hbs');
   });
+
+app.get('/', function (req, res) {
+  res.render('index.hbs');
+  });
+
+  app.post('/', function (req, res) {
+    console.log(req.body)
+    res.end()
+    });
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');

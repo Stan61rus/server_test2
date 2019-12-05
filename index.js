be43f58c74c1cdafaa78cfc14786e3958a2e4339
Konstantin Gorb <gorb@ucoz-team.net>
2019-12-05 16:49:24 +0300

    коментарии

--- index.js
+++ index.js
@@ -11,22 +11,25 @@ mongoose.connect('mongodb+srv://stan61rus:753538@cluster0-pmcuq.mongodb.net/test
   .then(() => console.log('MongoDB start'))
   .catch(err => console.log(err));
 
-  const User = mongoose.model('User', { email: String , phone: String });
+// Создаем модель данных для хранения в монге
+const User = mongoose.model('User', { email: String , phone: String });
 
 // Настройки конструктора Handlebars
 app.set('views', './views');
 app.set('view engine', 'hbs');
 
+// Показываем страничку по адресу
 app.get('/', function (req, res) {
   res.render('index.hbs');
   });
 
-  app.post('/', function (req, res) {
+app.post('/', function (req, res) {
   const user = new User({ email: req.body.email , phone: req.body.phone });
-  user.save().then(() => console.log('meow'));
+  user.save().then(() => console.log('ОК'));
   res.end()
-  });
+});
 
+// Запускаем приложение на порту 3000
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
   });
